a173dca7b5d21c6863436fab384e0a77fb8d3129
Mauro Bono <bono.mauro@gmail.com>
2023-01-19 20:59:58 +0100

    First commit for 42-theme WordPress starter theme

---
+++ README.md
@@ -0,0 +1,5 @@
+# 42 Theme
+
+This is a starting point for all 42Digital projects.
+
+It contains a real bare bones theme. Assets bundling is handled entirely by @wordpress/scripts.
---
+++ themes/42-theme/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package FortyTwoTheme
+ */
+?>
+	<?php if ( have_posts() ) : ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<h2><?php the_title(); ?></h2>
+			<?php the_content(); ?>
+		<?php endwhile; ?>
+	<?php endif; ?>
